f83ac5fbf014fe3dc3541ea5f75e13cbcc69632a
afmurillo97 <afmurillo97@gmail.com>
2024-06-19 15:16:16 -0500

    Modify pagination to 6 elements per page

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,7 +9,7 @@ class PostController extends Controller
 {
     public function index() {
         return view('index', [
-            'posts' => Post::latest()->paginate()
+            'posts' => Post::latest()->paginate(6)
         ]);
     }
 }
