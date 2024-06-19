d1d9b4c8b23f0b8b90c176f4a201f6abc989c906
afmurillo97 <afmurillo97@gmail.com>
2024-06-19 10:34:00 -0500

    Create routes for accessing Swagger documentation for both API versions

--- routes/api.php
+++ routes/api.php
@@ -9,11 +9,6 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
 */
 
 Route::post('login', [
@@ -21,24 +16,34 @@ Route::post('login', [
     'login'
 ]);
 
-// Public Routes V1
-Route::get('v1/posts', [PostV1::class, 'index']);
-Route::get('v1/posts/{post}', [PostV1::class, 'show']);
+Route::prefix('v1')->group(function () {
+    // Posts Routes
+    Route::prefix('posts')->group(function () {
+        // Public Routes V1
+        Route::get('/', [PostV1::class, 'index']);
+        Route::get('/{post}', [PostV1::class, 'show']);
 
-// Protected Routes V1
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('v1/posts', [PostV1::class, 'store']);
-    Route::put('v1/posts/{post}', [PostV1::class, 'update']);
-    Route::delete('v1/posts/{post}', [PostV1::class, 'destroy']);
+        // Protected Routes V1
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/', [PostV1::class, 'store']);
+            Route::put('/{post}', [PostV1::class, 'update']);
+            Route::delete('/{post}', [PostV1::class, 'destroy']);
+        });
+    });
 });
 
-// Public Routes V2
-Route::get('v2/posts', [PostV2::class, 'index']);
-Route::get('v2/posts/{post}', [PostV2::class, 'show']);
+Route::prefix('v2')->group(function () {
+    // Posts Routes
+    Route::prefix('posts')->group(function () {
+        // Public Routes V2
+        Route::get('/', [PostV2::class, 'index']);
+        Route::get('/{post}', [PostV2::class, 'show']);
 
-// Protected Routes V2
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('v2/posts', [PostV2::class, 'store']);
-    Route::put('v2/posts/{post}', [PostV2::class, 'update']);
-    Route::delete('v2/posts/{post}', [PostV2::class, 'destroy']);
+        // Protected Routes V2
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::post('/', [PostV2::class, 'store']);
+            Route::put('/{post}', [PostV2::class, 'update']);
+            Route::delete('/{post}', [PostV2::class, 'destroy']);
+        });
+    });
 });
